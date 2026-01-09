d2eb1aea4a6d8667225f0dfe53b1861221701888
Vishal Kamble <vishalkamble38396@gmail.com>
2026-01-09 15:07:34 +0530

    Fix API URL for production

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
+import api from "../services/api";
+
 import { motion } from "framer-motion";
 
 export default function Login() {
@@ -11,7 +13,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await api.post("/auth/login", {
         email,
         password,
       });
