3214fde8f3f8474310497b854bf0612149e334de
Vishal Kamble <vishalkamble38396@gmail.com>
2026-01-09 14:21:23 +0530

    Fix CORS origin slash issue

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://online-quize-mu.vercel.app/",
+      "https://online-quize-mu.vercel.app",
       "https://online-quize-8j80jwi0x-vishals-projects-a27808f4.vercel.app",// your Vercel frontend
       "http://localhost:5173"             // local dev
     ],
